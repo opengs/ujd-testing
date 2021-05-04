067b33711b4436ea6bee6decf2d57f2e07925ee5
Mykola Zhyhallo <mzhyhall@gmail.com>
2021-05-04 11:29:23 +0200

    Added rational reshape function

--- src/rational.py
+++ src/rational.py
@@ -9,6 +9,7 @@ class Rational():
     def __init__(self, numerator: int, denominator: int) -> None:
         self._numerator = numerator
         self._denominator = denominator
+        self.__reshape()
 
     @property
     def numerator(self) -> int:
@@ -26,6 +27,7 @@ class Rational():
         new_numerator = self._numerator*other.denominator + other.numerator*self.denominator
         self._numerator = new_numerator
         self._denominator = new_denominator
+        self.__reshape()
         return self
 
     def minus(self, other: Rational) -> Rational:
@@ -33,21 +35,25 @@ class Rational():
         new_numerator = self._numerator*other.denominator - other.numerator*self.denominator
         self._numerator = new_numerator
         self._denominator = new_denominator
+        self.__reshape()
         return self
 
-    def time(self, other: Rational) -> Rational:
+    def times(self, other: Rational) -> Rational:
         self._numerator = self._numerator*other.numerator
         self._denominator = self._denominator*other.denominator
+        self.__reshape()
         return self
 
     def divides(self, other: Rational) -> Rational:
         self._numerator = self._numerator*other.denominator
         self._denominator = self._denominator*other.numerator
+        self.__reshape()
         return self
 
     def mediant(self, other: Rational) -> Rational:
         self._numerator = self._numerator + other.numerator
         self._denominator = self._denominator + other.denominator
+        self.__reshape()
         return self
 
     def compareTo(self, other: Rational) -> int:
@@ -74,5 +80,17 @@ class Rational():
         self._numerator = self.denominator
         self._denominator = tmp
         return self
+
+    def __reshape(self):
+        a = abs(self.numerator)
+        b = abs(self.denominator)
+        while a*b != 0:
+            if a >= b:
+                a = a % b
+            else:
+                b = b % a
+        divider = a + b
+        self._numerator = round(self._numerator / divider)
+        self._denominator = round(self._denominator / divider)
 
     
--- tests/test_rational.py
+++ tests/test_rational.py
@@ -1,7 +1,14 @@
 import pytest
 
+from src.rational import Rational
 
 @pytest.mark.parametrize("numerator,denominator,expected_numerator,expected_denominator", [
     (10, 10, 1, 1),
+    (10, 5, 2, 1),
+    (10, -10, -1, 1),
+    (1, 2, 1, 2),
 ])
 def test_create(self, numerator, denominator, expected_numerator, expected_denominator):
+    rational = Rational(numerator, denominator)
+    assert rational.numerator == expected_numerator
+    assert rational.denominator == expected_denominator
